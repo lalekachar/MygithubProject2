d017d90c7272c84465589f39e2b0760349a2ba16
lalekachar <lalekachar@gmail.com>
2023-02-13 15:45:40 -0500

    secomd classes changed

--- src/Dat2GitHub/SecondGithubClass.java
+++ src/Dat2GitHub/SecondGithubClass.java
@@ -3,5 +3,6 @@ package Dat2GitHub;
 public class SecondGithubClass {
     public static void main(String[] args) {
         System.out.println("I am learning github");
+        System.out.println("2+2");
     }
 }
